b5f1b4c677ba4380bf602ea0f218c85eea01ecab
xianqq <xianqq@eg-group.cn>
2019-02-28 15:39:33 +0800

    增加解析过程的错误捕获

--- src/parser/CocosParser.ts
+++ src/parser/CocosParser.ts
@@ -4,7 +4,7 @@ import xml2js = require('xml2js');
 import fs = require('fs');
 
 export class CocosParser implements IParser {
-    
+
     parse(configFilePath: string): Promise<ITrimData> {
         return new Promise<ITrimData>((resolve, reject) => {
             fs.readFile(configFilePath, "utf-8", (err, data) => {
@@ -23,48 +23,53 @@ export class CocosParser implements IParser {
                         reject(err);
                         return;
                     }
-                    let content = json.plist.dict.dict[0];
-                    let frames = content.dict;
-                    let names = content.key;
-                    let n, item: ITrimItemData, arr, s: string, frame, key;
-                    for (let i = 0; i < names.length; i++) {
-                        plistData.itemDatas[i] = item = { name: names[i], rotated: false, degree: 0, frame: null, sourceColorRect: null, sourceSize: null };
-                        let frame = frames[i];
-                        let keys = frame.key;
-                        let strings = frame.string;
-                        n = 0;
-                        for (let j = 0; j < keys.length; j++) {
-                            key = keys[j];
-                            if (key == "rotated") {
-                                item.rotated = frame.true != undefined;
-                                item.degree = 270;
-                                continue;
+                    try {
+                        let content = json.plist.dict.dict[0];
+                        let frames = content.dict;
+                        let names = content.key;
+                        let n, item: ITrimItemData, arr, s: string, frame, key;
+                        for (let i = 0; i < names.length; i++) {
+                            plistData.itemDatas[i] = item = { name: names[i], rotated: false, degree: 0, frame: null, sourceColorRect: null, sourceSize: null };
+                            let frame = frames[i];
+                            let keys = frame.key;
+                            let strings = frame.string;
+                            n = 0;
+                            for (let j = 0; j < keys.length; j++) {
+                                key = keys[j];
+                                if (key == "rotated") {
+                                    item.rotated = frame.true != undefined;
+                                    item.degree = 270;
+                                    continue;
+                                }
+                                s = strings[n];
+                                arr = s.replace(/\{|\}/g, "").split(",");
+                                switch (key) {
+                                    case "frame":
+                                        Utils.strArr2NumArr(arr);
+                                        item.frame = arr;
+                                        break;
+                                    case "offset":
+                                        break;
+                                    case "sourceColorRect":
+                                        Utils.strArr2NumArr(arr);
+                                        item.sourceColorRect = arr;
+                                        break;
+                                    case "sourceSize":
+                                        Utils.strArr2NumArr(arr);
+                                        item.sourceSize = arr;
+                                        break;
+                                }
+                                ++n;
                             }
-                            s = strings[n];
-                            arr = s.replace(/\{|\}/g, "").split(",");
-                            switch (key) {
-                                case "frame":
-                                    Utils.strArr2NumArr(arr);
-                                    item.frame = arr;
-                                    break;
-                                case "offset":
-                                    break;
-                                case "sourceColorRect":
-                                    Utils.strArr2NumArr(arr);
-                                    item.sourceColorRect = arr;
-                                    break;
-                                case "sourceSize":
-                                    Utils.strArr2NumArr(arr);
-                                    item.sourceSize = arr;
-                                    break;
+                            if (item.rotated) {
+                                item.frame[2] = item.frame[2] ^ item.frame[3];
+                                item.frame[3] = item.frame[2] ^ item.frame[3];
+                                item.frame[2] = item.frame[2] ^ item.frame[3];
                             }
-                            ++n;
                         }
-                        if (item.rotated) {
-                            item.frame[2] = item.frame[2] ^ item.frame[3];
-                            item.frame[3] = item.frame[2] ^ item.frame[3];
-                            item.frame[2] = item.frame[2] ^ item.frame[3];
-                        }
+                    } catch (e) {
+                        reject(e);
+                        return;
                     }
                     resolve(plistData);
                 });
--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var unpacker = require("..");
 var path = require('path');
 
-unpacker.unpack(path.resolve("test", "ui"), "cc");
+// unpacker.unpack(path.resolve("test", "ui"), "cc");
+unpacker.unpack("/Users/xqq/mine/yd/assets", "cc");
