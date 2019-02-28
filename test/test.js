caa25b7fbfc63919f799d675a707a18e8f7ba499
xianqq <xianqq@eg-group.cn>
2019-02-28 15:39:58 +0800

    ..

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 var unpacker = require("..");
 var path = require('path');
 
-// unpacker.unpack(path.resolve("test", "ui"), "cc");
-unpacker.unpack("/Users/xqq/mine/yd/assets", "cc");
+unpacker.unpack(path.resolve("test", "ui"), "cc");
